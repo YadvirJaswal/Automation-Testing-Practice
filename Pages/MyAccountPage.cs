f9dc7586f8fbf62164b5130d8663dd8629034b90
Yadvir Jaswal <yadvirjaswal@gmail.com>
2024-11-20 11:00:31 +1300

    User Login and Dashboard Access Validation.
    This test case verifies the functionality of user login and access to the dashboard on the website. It ensures that registered users can successfully log in and navigate to the dashboard without errors.

---
+++ Pages/MyAccountPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace AutomationPracticeSiteProject.Pages
+{
+    public class MyAccountPage
+    {
+        private readonly IWebDriver driver;
+        public MyAccountPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private List<IWebElement> GetMyAccountNavigations(int liIndex)
+        {
+            // Get My Account naviagtion class element
+            var myAccountNavigationsElement = driver.FindElement(By.ClassName("woocommerce-MyAccount-navigation"));
+            // Get all the lists of options of navigation
+            var optionsLists = myAccountNavigationsElement.FindElements(By.TagName("li")).ToList();
+            var optionList = optionsLists[liIndex];
+            // Get anchor tags which contains the link of option
+            var optionAnchorTags = optionList.FindElements(By.TagName("a")).ToList();
+            return optionAnchorTags;
+        }
+        public void ValidateDashboardFunctionality()
+        {
+            var optionAnchorTags = GetMyAccountNavigations(0);
+
+            // Get and click on dashboard option
+            var lnkDashboard = optionAnchorTags[0];
+            lnkDashboard.Click();
+
+            // Get the dashboard content
+            var dashboardContentElement = driver.FindElement(By.ClassName("woocommerce-MyAccount-content"));
+            // Get the paragraphs in content
+            var dashboardContentParagraphs = dashboardContentElement.FindElements(By.TagName("p"));
+            // Get the text of first paragraph
+            var actualText = dashboardContentParagraphs[0].Text;
+            var expectedText = "Hello sumit2 (not sumit2? Sign out)";
+            //Assert the text displayed on the dashboard
+            Assert.Equal(expectedText, actualText);
+        }
+    }
+}
